1e71f93da7e55260813ca5ec706a844ee8dbffa3
FKay12 <38141648+FKay12@users.noreply.github.com>
2022-07-09 13:59:13 -0700

    Script

---
+++ script.js
@@ -0,0 +1,15 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark **2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Marks's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+}
+else {
+    console.log(`Johns's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
